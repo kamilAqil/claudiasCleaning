93c13775fe5a7a35674c392124c5a572888df4d0
Kamil <kaqil91@gmail.com>
2017-10-12 20:10:58 -0700

    moving the image from the background property to an img tag to keep the border res

--- client/components/QuoteComponent.js
+++ client/components/QuoteComponent.js
@@ -3,16 +3,19 @@ import BackgroundImage from '../images/cleanHousePicOne.jpg';
 
 let containerStyle = {
     height:'360px',
-    width:'100%',
-    border:'2px solid #F0FF79',
-    background:'url(/images/cleanHousePicOne.jpg) no-repeat center'
+    width:'369px',
+    margin:'auto',
+    marginTop:'10px',
+    border:'2px solid #F0FF79'
+    // background:'url(/images/cleanHousePicOne.jpg) no-repeat center',
+    // filter: 'blur(5px)'
 };
 
 class QuoteComponent extends React.Component{
     render(){
         return(
             <div style={containerStyle}>
-                {/* <img src='/images/cleanHousePicOne.jpg'></img> */}
+                 <img src='/images/cleanHousePicOne.jpg'></img>
             </div>
         )
     }
