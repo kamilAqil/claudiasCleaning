a1f2ddfd0e17a1699657bc76e9236b154853d8b4
Kamil <kaqil91@gmail.com>
2017-10-16 17:11:36 -0700

    loggin change on form input

--- client/components/QuoteComponent.js
+++ client/components/QuoteComponent.js
@@ -6,16 +6,82 @@ let containerStyle = {
     width:'369px',
     margin:'auto',
     marginTop:'10px',
-    border:'2px solid #F0FF79'
+    border:'2px solid #F0FF79',
+    position:'relative',
+    borderRadius:'10px',
+    overflow:'hidden'
     // background:'url(/images/cleanHousePicOne.jpg) no-repeat center',
     // filter: 'blur(5px)'
 };
 
+let roomPictureBackgroundStyle = {
+    position: 'absolute',
+    width:'100%',
+    height:'100%',
+    borderRadius:'10px',
+    top:'0px',
+    left:'0px',
+    zIndex:'0'
+}
+
+let formBackgroundStyle = {
+    width:'95%',
+    height:'95%',
+    position:'absolute',
+    position:'relative',
+    backgroundColor:'white',
+    top: '2.5%',
+    left: '2.5%',
+    zIndex:'1',
+    borderRadius:'10px',
+    filter:'blur(5px) brightness(120%) opacity(76%)'
+}
+
+let formStyle = {
+    position:'absolute',
+    top: '2.5%',
+    left: '2.5%',
+    zIndex:'1',
+    textAlign:'center'
+}
+
+
 class QuoteComponent extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            firstName : '',
+            lastName:''
+        };
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event){
+        // console.log(event.target.name);
+        var nameToChange = event.target.name;
+        // console.log(typeof nameToChange);
+        // console.log(nameToChange);
+        // console.log(event.target.value);
+        this.setState({[nameToChange]: event.target.value});
+        console.log(this.state);
+    }
+
+    
+
     render(){
         return(
             <div style={containerStyle}>
-                 <img src='/images/cleanHousePicOne.jpg'></img>
+                <img src='/images/cleanHousePicOne.jpg' style={roomPictureBackgroundStyle}></img>
+                <div style={formBackgroundStyle}/>
+                <form style={formStyle}>
+                    <div>
+                        <textarea name='firstName' placeholder='First Name' onChange={this.handleChange} value={this.state.firstName} />
+                    </div>
+                    <div>
+                        <textarea name='lastName' placeholder='Last Name' onChange={this.handleChange} />
+                    </div>
+                </form>
             </div>
         )
     }
